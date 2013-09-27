29f6bdf448e8968be3afe51c8a68f976406bf7f6
Matthew Schick <matthew.schick@gmail.com>
2013-09-26 20:32:59 -0400

    Update bed size for new x-axis
    Signed-off-by: Matthew Schick <matthew.schick@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -284,11 +284,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define min_software_endstops true // If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 // Travel limits after homing
-#define X_MAX_POS 114
+#define X_MAX_POS 119
 #define X_MIN_POS 0
-#define Y_MAX_POS 121
+#define Y_MAX_POS 130
 #define Y_MIN_POS 0
-#define Z_MAX_POS 138
+#define Z_MAX_POS 140
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
