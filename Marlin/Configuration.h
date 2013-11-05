8418aab89b22b42226fe1a4440bc32a7cf578981
Matthew Schick <matthew.schick@gmail.com>
2013-11-05 12:30:56 -0500

    Pretty sure we want zjerk here
    Signed-off-by: Matthew Schick <matthew.schick@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -19,7 +19,7 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "nophead" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Matthew Schick" // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
@@ -385,7 +385,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // The speed change that does not require acceleration (i.e. the software might assume it can be done instantaneously)
 #define DEFAULT_XYJERK                10.0    // (mm/sec)
-#define DEFAULT_ZJERK                 0.0     // (mm/sec)
+#define DEFAULT_ZJERK                 0.4     // (mm/sec)
 #define DEFAULT_EJERK                 10.0    // (mm/sec)
 
 //===========================================================================
