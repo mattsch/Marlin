7cecfcd0fff512ce8d3d9116efed8c2e896f4bba
Matthew Schick <matthew.schick@gmail.com>
2014-02-20 22:55:28 -0500

    Update esteps, moron is I
    Signed-off-by: Matthew Schick <matthew.schick@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -422,7 +422,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,200*16/1,563.99}  // Mendel90 with Wades and Stoffel15 bolt 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,200*16/1,501.95}  // Mendel90 with Wades and Stoffel15 bolt 
 #define DEFAULT_MAX_FEEDRATE          {400, 400, 4, 30}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {2000,2000,150,5000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
