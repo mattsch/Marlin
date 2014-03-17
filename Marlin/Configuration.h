bc243ad46e5a85fa6930b790ac528e3704dbd341
Matthew Schick <matthew.schick@gmail.com>
2014-03-17 17:07:13 -0400

    Update Z_HOME_POS
    Signed-off-by: Matthew Schick <matthew.schick@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -442,7 +442,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // For deltabots this means top and center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS (X_MIN_POS - 1)
 #define MANUAL_Y_HOME_POS (Y_MIN_POS - 1)
-#define MANUAL_Z_HOME_POS (203.96)
+#define MANUAL_Z_HOME_POS (203.87)
 //#define MANUAL_Z_HOME_POS 402 // For delta: Distance between nozzle and print surface after homing.
 
 //// MOVEMENT SETTINGS
